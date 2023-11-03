900b7e5d7b47e6fc62562a231f2097d124d74c92
kolosik <ilazabugin@gmail.com>
2023-11-04 00:09:40 +0300

    modified:   maze/src/App.svelte

--- maze/src/App.svelte
+++ maze/src/App.svelte
@@ -2,16 +2,6 @@
   import svelteLogo from './assets/svelte.svg'
   import viteLogo from '/vite.svg'
   import Counter from './lib/Counter.svelte'
-  export let calories = 0;
-  let gender = Number((<HTMLSelectElement>document.getElementById('gender')).value);
-  if (gender == 1){
-    //(10 × вес в килограммах) + (6,25 × рост в сантиметрах) − (5 × возраст в годах) + 5
-
-  }
-  else{
-    //Для женщин: (10 × вес в килограммах) + (6,25 × рост в сантиметрах) − (5 × возраст в годах) − 161
-
-  }
 </script>
 
 <main>
@@ -19,17 +9,17 @@
     <div class="input">
       <div class = "height">
         <p>Рост</p>
-        <input type="float">
+        <input type="number" id = "height">
       </div>
   
       <div class = "weight">
         <p>Вес</p>
-        <input type="float">
+        <input type="number" id = "weight">
       </div>
   
       <div class = "age">
         <p>Возраст</p>
-        <input type="float">
+        <input type="number" id = "age">
       </div>
     </div>
 
@@ -56,30 +46,34 @@
           <option value="1.9">2 раза в день</option>
         </select>
       </div>
+<script lang="ts">
+  function myFunction() {
+    let gender = Number((<HTMLSelectElement>document.getElementById('gender')).value);
+    var height = document.getElementById("height").value;
+    var weight = document.getElementById("weight").value;
+    var age = document.getElementById("age").value
 
+    if (gender == 1){
+    //(10 × вес в килограммах) + (6,25 × рост в сантиметрах) − (5 × возраст в годах) + 5
+      calories = (10*weight) + (6,25 * height) - (5*age) + 5;
+
+    }
+    else{
+
+      calories = (10*weight) + (6,25 * height) - (5*age);
+      calories = calories - 165
+    }
+    document.getElementById('calories').innerHTML=calories + "Ккал";
+};
+</script>
     </div>
     <div class="final">
-      <label for="calories">
-        {calories} Ккал 
-      </label>
+      <button value="Сколько нужно калорий в день" onclick="myFunction()"> Сколько нужно калорий в день </button>
+      <p id= "calories">
+      </p>
     </div>
   </div>
 </main>
 
 <style>
-  .logo {
-    height: 6em;
-    padding: 1.5em;
-    will-change: filter;
-    transition: filter 300ms;
-  }
-  .logo:hover {
-    filter: drop-shadow(0 0 2em #646cffaa);
-  }
-  .logo.svelte:hover {
-    filter: drop-shadow(0 0 2em #ff3e00aa);
-  }
-  .read-the-docs {
-    color: #888;
-  }
 </style>
